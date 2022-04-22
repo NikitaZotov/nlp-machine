0dbf68710c55d5a9f836568f3d89d04f65a70f96
NikitaZotov <nikita.zotov.belarus@gmail.com>
2022-04-22 15:32:32 +0300

    feat(server): implement server

---
+++ external-modules/server/params.py
@@ -0,0 +1,14 @@
+"""
+    Author Zotov Nikita
+"""
+
+FLASK_IP = "127.0.0.1"
+FLASK_PORT = 5000
+PLATFORM_IP = "127.0.0.1"
+PLATFORM_PORT = 8090
+PLATFORM_URL_PATH = "ws_json"
+
+DEFAULT_TIMEOUT = 3
+
+HTTP = "http://"
+HTTPS = "https://"
